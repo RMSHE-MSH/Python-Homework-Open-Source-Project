757788cf56314237315839dc6348bb2aab52a5c1
RMSHE <asdfghjkl851@outlook.com>
2022-10-03 13:40:50 +0800

    2022.10.03

--- EasyX Graphics Library for Python/EasyX Graphics Library for Python/EasyXGraphicsFunction.cpp
+++ EasyX Graphics Library for Python/EasyX Graphics Library for Python/EasyXGraphicsFunction.cpp
@@ -3,10 +3,10 @@
 
 void c_cleardevice() { cleardevice(); }
 
-HWND c_initgraph(int width, int height, int flag) {
+HWND c_initgraph(int width, int height, int flag, COLORREF bkcolor) {
 	HWND hWnd = initgraph(width, height, flag);
 	setorigin(0, 0);
-	setbkcolor(RGB(30, 30, 30));
+	setbkcolor(bkcolor);
 	cleardevice();
 	setaspectratio(1, 1);
 	::setbkmode(TRANSPARENT);
@@ -33,6 +33,50 @@ COLORREF c_HSVtoRGB(float H, float S, float V) { return HSVtoRGB(H, S, V); }
 
 COLORREF c_RGB(BYTE byRed, BYTE byGreen, BYTE byBlue) { return RGB(byRed, byGreen, byBlue); }
 
-void c_putpixel(int x, int y, COLORREF color) { putpixel(x, y, color); }
+COLORREF c_RGBtoGRAY(COLORREF rgb) { return RGBtoGRAY(rgb); }
+
+COLORREF c_getbkcolor() { return getbkcolor(); }
+
+int c_getbkmode() { return getbkmode(); }
+
+COLORREF c_getfillcolor() { return getfillcolor(); }
+
+COLORREF c_getlinecolor() { return getlinecolor(); }
+
+void c_setbkcolor(COLORREF color) { setbkcolor(color); }
+
+void c_setbkmode(int mode) { setbkmode(mode); }
+
+void c_setfillcolor(COLORREF color) { setfillcolor(color); }
+
+void c_setfillstyle(int style, long hatch, IMAGE *ppattern) { setfillstyle(style, hatch, ppattern); }
+
+void c_setlinecolor(COLORREF color) { setlinecolor(color); }
+
+void c_setlinestyle(int style, int thickness, const DWORD *puserstyle, DWORD userstylecount) { setlinestyle(style, thickness, puserstyle, userstylecount); }
+
+void c_arc(int left, int top, int right, int bottom, double stangle, double endangle) { arc(left, top, right, bottom, stangle, endangle); }
 
 void c_circle(int x, int y, int radius) { circle(x, y, radius); }
+
+void c_clearcircle(int x, int y, int radius) { clearcircle(x, y, radius); }
+
+void c_clearellipse(int left, int top, int right, int bottom) { clearellipse(left, top, right, bottom); }
+
+void c_clearpie(int left, int top, int right, int bottom, double stangle, double endangle) { clearpie(left, top, right, bottom, stangle, endangle); }
+
+void c_clearpolygon(const POINT *points, int num) { clearpolygon(points, num); }
+
+void c_clearrectangle(int left, int top, int right, int bottom) { clearrectangle(left, top, right, bottom); }
+
+void c_clearroundrect(int left, int top, int right, int bottom, int ellipsewidth, int ellipseheight) { clearroundrect(left, top, right, bottom, ellipsewidth, ellipseheight); }
+
+void c_ellipse(int left, int top, int right, int bottom) { ellipse(left, top, right, bottom); }
+
+void c_fillcircle(int x, int y, int radius) { fillcircle(x, y, radius); }
+
+void c_fillellipse(int left, int top, int right, int bottom) { fillellipse(left, top, right, bottom); }
+
+void c_fillpie(int left, int top, int right, int bottom, double stangle, double endangle) { fillpie(left, top, right, bottom, stangle, endangle); }
+
+void c_putpixel(int x, int y, COLORREF color) { putpixel(x, y, color); }
--- EasyX Graphics Library for Python/EasyX Graphics Library for Python/EasyXGraphicsFunction.h
+++ EasyX Graphics Library for Python/EasyX Graphics Library for Python/EasyXGraphicsFunction.h
@@ -5,9 +5,10 @@
 #define EASYX_API extern "C" __declspec(dllexport)
 
 //绘图设备相关函数;
+
 EASYX_API void c_cleardevice();
 
-EASYX_API HWND c_initgraph(int width, int height, int flag = EW_SHOWCONSOLE);
+EASYX_API HWND c_initgraph(int width, int height, int flag = EW_SHOWCONSOLE, COLORREF bkcolor = RGB(40, 44, 52));
 
 EASYX_API void c_closegraph();
 
@@ -18,6 +19,7 @@ EASYX_API void c_graphdefaults();
 EASYX_API void c_setorigin(int x, int y);
 
 //颜色模型;
+
 EASYX_API BYTE c_GetBValue(COLORREF rgb);
 
 EASYX_API BYTE c_GetGValue(COLORREF rgb);
@@ -30,6 +32,53 @@ EASYX_API COLORREF c_HSVtoRGB(float H, float S, float V);
 
 EASYX_API COLORREF c_RGB(BYTE byRed, BYTE byGreen, BYTE byBlue);
 
-EASYX_API void c_putpixel(int x, int y, COLORREF color);
+EASYX_API COLORREF c_RGBtoGRAY(COLORREF rgb);
+
+//图形颜色及样式设置相关函数;
+
+EASYX_API COLORREF c_getbkcolor();
+
+EASYX_API int c_getbkmode();
+
+EASYX_API COLORREF c_getfillcolor();
+
+EASYX_API COLORREF c_getlinecolor();
+
+EASYX_API void c_setbkcolor(COLORREF color = RGB(40, 44, 52));
+
+EASYX_API void c_setbkmode(int mode = TRANSPARENT);
+
+EASYX_API void c_setfillcolor(COLORREF color = RGB(239, 239, 239));
+
+EASYX_API void c_setfillstyle(int style = BS_SOLID, long hatch = NULL, IMAGE *ppattern = NULL);
+
+EASYX_API void c_setlinecolor(COLORREF color = RGB(239, 239, 239));
+
+EASYX_API void c_setlinestyle(int style = PS_SOLID | PS_ENDCAP_ROUND, int thickness = 1, const DWORD *puserstyle = NULL, DWORD userstylecount = 0);
+
+//图形绘制相关函数;
+EASYX_API void c_arc(int left, int top, int right, int bottom, double stangle, double endangle);
 
 EASYX_API void c_circle(int x, int y, int radius);
+
+EASYX_API void c_clearcircle(int x, int y, int radius);
+
+EASYX_API void c_clearellipse(int left, int top, int right, int bottom);
+
+EASYX_API void c_clearpie(int left, int top, int right, int bottom, double stangle, double endangle);
+
+EASYX_API void c_clearpolygon(const POINT *points, int num);
+
+EASYX_API void c_clearrectangle(int left, int top, int right, int bottom);
+
+EASYX_API void c_clearroundrect(int left, int top, int right, int bottom, int ellipsewidth, int ellipseheight);
+
+EASYX_API void c_ellipse(int left, int top, int right, int bottom);
+
+EASYX_API void c_fillcircle(int x, int y, int radius);
+
+EASYX_API void c_fillellipse(int left, int top, int right, int bottom);
+
+EASYX_API void c_fillpie(int left, int top, int right, int bottom, double stangle, double endangle);
+
+EASYX_API void c_putpixel(int x, int y, COLORREF color);
